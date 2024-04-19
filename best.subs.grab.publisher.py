5fed9c3729b04354d343bc22938b892d9bbca733
lex <7863785+singulart@users.noreply.github.com>
2024-04-19 14:08:42 -0400

    Renamed files

--- best.subs.grab.publisher.py
+++ best.subs.grab.publisher.py
@@ -2,7 +2,7 @@ from savebestcommunities import save_best_communities
 
 def main():
 
-    for i in range(1, 1238): # 1238 is the last page as of April 2024
+    for i in range(1, 1285): # 1285 is the last page as of April 2024
         save_best_communities.delay(i) # push to Redis
 
     print(f'Published messages to Redis. All OK.')
