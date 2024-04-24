01345ae483d9ce566fd72e8e33322103eaaee461
singulart <serpian@gmail.com>
2024-04-24 14:47:02 -0400

    Categorized 20k more subs after discovering a bug in best subs grabber

--- qa/test_best_subs_publisher.py
+++ qa/test_best_subs_publisher.py
@@ -5,21 +5,14 @@ import sqlite3
 import requests
 import re
 import datetime
-from celery import Celery
 from celery.utils.log import get_task_logger
+import logging
 
-app = Celery(config_source='celeryconfig')
 logger = get_task_logger(__name__)
+logging.basicConfig(level = logging.INFO)
 
-@app.task(name='Save Best Reddits')
 def save_best_communities(pagenum):
     
-    regex_pattern = r'(\/r\/\w+)'
-
-    # Establish a connection to the SQLite database file
-    conn = sqlite3.connect('reddit.db')
-    # Create a cursor object using the connection
-    cur = conn.cursor()
 
     # Create a table (if it does not already exist)
     cur.execute('CREATE TABLE IF NOT EXISTS reddit_subs_best (sub VARCHAR(64))')
@@ -31,10 +24,24 @@ def save_best_communities(pagenum):
         return 
             
     sub_names_matches = [match for match in re.findall(regex_pattern, subs_response.text)]
-    sub_names = [(s, ) for s in set(sub_names_matches)]
-    # logger.info('HTTP call + regexp took {} ms.'.format((datetime.datetime.now() - now).total_seconds()))
+    sub_names = [f"'{s}'" for s in set(sub_names_matches)]
     
-    # This produces 1300+ duplicated records, see README.md on how to dedup using SQL
-    cur.executemany('INSERT INTO reddit_subs_best (sub) VALUES (?)', sub_names)
-    conn.commit()
-    conn.close()
+    sql_st = f"select sub from reddit_subs_best where sub in ({','.join(sub_names)})"
+    # logger.info(sql_st)
+    cur.execute(sql_st)
+    db_results = cur.fetchall()
+    logger.info(f'On page {pagenum} got {len(sub_names)} unique subs, from which {len(sub_names) - len(db_results)} NOT found in DB')
+
+if __name__ == "__main__":
+    regex_pattern = r'(\/r\/\w+)'
+
+    # Establish a connection to the SQLite database file
+    conn = sqlite3.connect('reddit.db')
+    # Create a cursor object using the connection
+    cur = conn.cursor()
+
+    for i in range(1, 682): 
+        save_best_communities(i)
+        
+        
+    cur.close()
